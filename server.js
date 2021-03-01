7fd0e62219ad14300872966fd6493313dd59ebbb
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-03-01 18:34:23 -0500

    Launched server

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express');
+require('dotenv').config();
+const PORT = 3001;
+
+const app = express();
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended: true}))
+
+app.get('/hello',(req,res)=>{
+    res.render('./pages/index.ejs');
+})
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
